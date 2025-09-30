4f53858cfd3fb60bf1393a751d2eb25e3da4f510
Veikko Pankakoski <veikko@rautanenyhtiot.fi>
2025-09-30 13:30:05 +0300

    Upgrade old lxml library [ci skip]

--- netbox_panorama_configpump_plugin/__init__.py
+++ netbox_panorama_configpump_plugin/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 __author__ = """Veikko Pankakoski"""
 __email__ = "veikko@rautanen.io"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 
 from netbox.plugins import PluginConfig
