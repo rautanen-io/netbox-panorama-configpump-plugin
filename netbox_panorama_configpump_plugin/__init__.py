92685d72f90d4c1f76f852092d8d6e8ea71e2614
Veikko Pankakoski <veikko@rautanenyhtiot.fi>
2025-11-03 15:28:59 +0200

    Update correct version number to __init__.py

--- netbox_panorama_configpump_plugin/__init__.py
+++ netbox_panorama_configpump_plugin/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 __author__ = """Veikko Pankakoski"""
 __email__ = "veikko@rautanen.io"
-__version__ = "1.0.2"
+__version__ = "1.0.6"
 
 
 from netbox.plugins import PluginConfig
